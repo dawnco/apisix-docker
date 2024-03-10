37363456dca276d98fd6b933cdfb055e4f9c9666
Daw <abke@qq.com>
2024-03-10 14:54:12 +0800

    add

--- apisix-wms/www/index.php
+++ apisix-wms/www/index.php
@@ -2,7 +2,12 @@
 
 date_default_timezone_set("Asia/Shanghai");
 
+file_put_contents(__DIR__ . "/temp/json_" . date("Ymd_His") . ".log", file_get_contents("php://input"));
+
 file_put_contents(__DIR__ . "/temp/post_" . date("Ymd_His") . ".log", json_encode($_POST));
 
 
 file_put_contents(__DIR__ . "/temp/request_" . date("Ymd_His") . ".log", json_encode($_REQUEST));
+
+
+file_put_contents(__DIR__ . "/temp/server_" . date("Ymd_His") . ".log", json_encode($_SERVER));
